6a3359532c6aadf70c4c1a659debe0317686e24a
Pavel <pavel@b2profit.ru>
2024-01-12 00:32:19 +0300

    Сделал домашку

--- Seminars/Seminar3.py
+++ Seminars/Seminar3.py
@@ -55,8 +55,8 @@ Input: [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"},
 Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
 """
 
-lst_obj = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"},
-           {"VII": "S005"}, {"V": "S009"}, {"VIII": "S007"}]
+#lst_obj = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"},
+ #          {"VII": "S005"}, {"V": "S009"}, {"VIII": "S007"}]
 '''
 #ТРАДИЦИОННЫЙ ИТЕРАТОР
 new_set = set()
@@ -67,7 +67,7 @@ for el in lst_obj:
 print(new_set)
 '''
 #ВКЛЮЧЕНИЕ
-print(set(val for el in lst_obj for val in el.values()))
+#print(set(val for el in lst_obj for val in el.values()))
 '''
 
 Задача №23. Решение в группах
@@ -99,4 +99,132 @@ for i in range(0, len(lst_1),2):
         res_02.append(lst_1[i])
 print(res_02)
 '''
+# ДОМАШКА
 
+#1 ЗАДАЧА:
+'''
+Требуется вычислить, сколько раз встречается некоторое число k в массиве list_1.
+Найдите количество и выведите его.
+Пример:
+list_1 = [1, 2, 3, 4, 5]
+k = 3
+# 1
+'''
+'''
+list_1 = [1, 2, 3, 4, 5]
+k = 3
+count = 0
+for element in list_1:
+    if element == k:
+        count += 1
+print(count)
+
+# ЭТАЛОН:
+count = 0
+for i in range(len(list_1)):
+    if list_1[i] == k:
+        count += 1
+print(count)
+'''
+
+#2 ЗАДАЧА:
+'''
+Требуется найти в массиве list_1 
+самый близкий по значению элемент к заданному числу k и вывести его.
+Считать, что такой элемент может быть только один. 
+Если значение k совпадает с этим элементом - выведите его.
+Пример:
+list_1 = [1, 2, 3, 4, 5]
+k = 6
+# 5
+'''
+'''
+list_1 = [1, 2, 3, 4, 5]
+k = 6
+delta = min([abs(val - k) for val in list_1])
+if (k - delta) in list_1:
+    print(k - delta)
+else:
+    print(k + delta)
+
+# ЭТАЛОН
+m = abs(k - list_1[0])  # модуль числа
+number = list_1[0]
+for i in range(1, len(list_1)):
+    if m > abs(list_1[i] - k):
+        m = abs(list_1[i] - k)
+        number = list_1[i]
+print(number)
+'''
+
+#3 ЗАДАЧА:
+'''
+Скрабл
+
+
+В настольной игре Скрабл (Scrabble) каждая буква имеет определенную ценность.
+В случае с английским алфавитом очки распределяются так:
+A, E, I, O, U, L, N, S, T, R – 1 очко;
+D, G – 2 очка;
+B, C, M, P – 3 очка;
+F, H, V, W, Y – 4 очка;
+K – 5 очков;
+J, X – 8 очков;
+Q, Z – 10 очков.
+А русские буквы оцениваются так:
+А, В, Е, И, Н, О, Р, С, Т – 1 очко;
+Д, К, Л, М, П, У – 2 очка;
+Б, Г, Ё, Ь, Я – 3 очка;
+Й, Ы – 4 очка;
+Ж, З, Х, Ц, Ч – 5 очков;
+Ш, Э, Ю – 8 очков;
+Ф, Щ, Ъ – 10 очков.
+Напишите программу, которая вычисляет стоимость введенного пользователем слова k и выводит его. 
+Будем считать, что на вход подается только одно слово, которое содержит либо только английские, либо только русские буквы.
+Пример:
+k = 'ноутбук'
+# 12
+'''
+'''
+k = 'ноутбук'
+word = k.upper()
+str_1 = 'AEIOULNSTRАВЕИНОРСТ'
+str_2 = 'DGДКЛМПУ'
+str_3 = 'BCMPБГЁЬЯ'
+str_4 = 'FHVWYЙЫ'
+str_5 = 'KЖЗХЦЧ'
+str_8 = 'JXШЭЮ'
+str_10 = 'QZФЩЪ'
+value = 0
+for letter in word:
+    if letter in str_1:
+        value += 1
+    elif letter in str_2:
+        value += 2
+    elif letter in str_3:
+        value += 3
+    elif letter in str_4:
+        value += 4
+    elif letter in str_5:
+        value += 5
+    elif letter in str_8:
+        value += 8
+    elif letter in str_10:
+        value += 10
+print(value)
+'''
+# ЭТАЛОН
+points_en = {1: 'AEIOULNSTR', 2: 'DG', 3: 'BCMP', 4: 'FHVWY', 5: 'K', 8: 'JX', 10: 'QZ'}
+points_ru = {1: 'АВЕИНОРСТ', 2: 'ДКЛМПУ', 3: 'БГЁЬЯ', 4: 'ЙЫ', 5: 'ЖЗХЦЧ', 8: 'ШЭЮ', 10: 'ФЩЪ'}
+word = k.upper()  # переводим все буквы в верхний регистр
+count = 0
+for i in word:
+    if i in 'QWERTYUIOPASDFGHJKLZXCVBNM':
+        for j in points_en:
+            if i in points_en[j]:
+                count = count + j
+    else:
+        for j in points_en:
+            if i in points_ru[j]:
+                count = count + j
+print(count)
