ecc270d7c216061434ae512b4140f22f673dd741
Pavel <pavel@b2profit.ru>
2024-01-16 23:20:58 +0300

    Продолжил запись 4 семинара

--- Seminars/Seminar4.py
+++ Seminars/Seminar4.py
@@ -80,12 +80,53 @@ I'm sure that the shells are sea shore shells
 Output: 
 13
 '''
+'''
 string = """She sells sea shells on the sea shore 
 The shells that she sells are sea shells 
 I'm sure.So if she sells sea shells on the sea shore 
 I'm sure that the shells are sea shore shells""".upper().split()
 # print(len(set(string.upper().split())))
 print(len(set(string)))
+'''
 
 # ctrl + alt + l  Нажать для исправления стиля
 # ctrl + / Закоментировать выделенное
+
+# Задача №27
+'''
+Задача №29. 
+Ваня и Петя поспорили, кто быстрее решит следующую задачу: 
+“Задана последовательность неотрицательных целых чисел. 
+Требуется определить значение наибольшего элемента последовательности, 
+которая завершается первым встретившимся нулем (число 0 не входит в последовательность)”. 
+Однако 2  друга оказались не такими смышлеными. 
+Никто из ребят не смог до конца сделать это задание. 
+Они решили так: у кого будет меньше ошибок в коде, тот и выиграл спор. 
+За помощью товарищи обратились к Вам, студентам.
+
+Ваня: 
+n = int(input()) 
+max_number = 1000 
+while n != 0:   
+    n = int(input())   
+    if max_number > n:       
+        max_number = n 
+print(max_number)
+
+Петя: 
+n = int(input()) 
+max_number = -1 
+while n < 0:   
+    n = int(input())   
+    if max_number < n:       
+        n = max_number 
+print(n)
+'''
+
+n = int(input())
+max_number = n
+while n != 0:
+    n = int(input())
+    if max_number < n:
+        max_number = n
+print(max_number)
